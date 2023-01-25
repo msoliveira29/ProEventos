8691966b627ca53b1391680bb6323ab1e9751485
Marcelo Oliveira <marcelo.oliveira@telehelp.com.br>
2023-01-25 02:58:26 -0300

    Laytout Responsivo + Forms

---
+++ Front/ProEventos-App/src/app/components/eventos/evento-lista/evento-lista.component.ts
@@ -0,0 +1,88 @@
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { Evento } from 'src/app/models/Evento';
+import { EventoService } from 'src/app/services/evento.service';
+
+@Component({
+  selector: 'app-evento-lista',
+  templateUrl: './evento-lista.component.html',
+  styleUrls: ['./evento-lista.component.scss']
+})
+export class EventoListaComponent implements OnInit {
+
+  modalRef?: BsModalRef;
+  public eventos: Evento[] = [];
+  public eventosFiltrados: Evento[] = [];
+  public larguraImagem: number = 150;
+  public margemImagem: number = 2;
+  public exibirImagem: boolean = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista() {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  public filtrarEventos(filtrarPor: string): Evento[] {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (x: { tema: string; local: string }) => x.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        x.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
+
+  constructor(
+    private eventoService: EventoService,
+    private modalService: BsModalService,
+    private toastrService: ToastrService,
+    private spinnerService: NgxSpinnerService,
+    private router: Router
+  ) { }
+
+  public ngOnInit(): void {
+    this.spinnerService.show();
+    this.getEventos();
+  }
+
+  public alterarImagem(): void {
+    this.exibirImagem = !this.exibirImagem;
+  }
+
+  public getEventos(): void {
+    this.eventoService.getEventos().subscribe({
+      next: (_eventos: Evento[]) => {
+        this.eventos = _eventos,
+          this.eventosFiltrados = this.eventos
+      },
+      error: (error: any) => {
+        this.spinnerService.hide();
+        this.toastrService.error('Erro ao carregar os Eventos.', 'Error!');
+      },
+      complete: () => this.spinnerService.hide()
+    });
+  }
+
+  openModal(template: TemplateRef<any>): void {
+    this.modalRef = this.modalService.show(template, { class: 'modal-sm' });
+  }
+
+  confirm(): void {
+    this.modalRef?.hide();
+    this.toastrService.success('O evento foi deletado com sucesso.', 'Deletado!');
+  }
+
+  decline(): void {
+    this.modalRef?.hide();
+  }
+
+  detalheEvento(id: number): void {
+    this.router.navigate([`eventos/detalhe/${id}`]);
+  }
+}
